424d468cf379b81b6430971d3911d5bf6878cd91
xiwen.yu <xiwen.yu@jiangrongxin.com>
2019-07-14 16:08:41 +0800

    第一次提交什么也没写

---
+++ src/test/java/com/yxw/student/StudentTest.java
@@ -0,0 +1,21 @@
+package com.yxw.student;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author:Yuxiwen
+ * @Description:
+ * @Date: 2019/7/14
+ * @Nodified By:
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentTest {
+    @Test
+    public void test01() {
+        System.out.println("hello");
+    }
+}
